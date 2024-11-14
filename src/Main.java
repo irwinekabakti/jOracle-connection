c0697079406f2a2fd7e402c5d8ff90d987f22202
irwinekabakti <irwinekabakti@gmail.com>
2024-11-14 11:14:49 +0800

    example insert data

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-//        String dbURL = "jdbc:oracle:thin://localhost:1521/orcl1";
         String dbURL = "jdbc:oracle:thin:system/lEAfs1967@localhost:1521:orcl1";
 
-//        String dbURL = "jdbc:oracle:oci@localhost:1521:orcl1";
-//        String username = "system";
-//        String password = "lEAfs1967";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
         try {
-            Connection connection = DriverManager.getConnection(dbURL);
-//            Connection connection = DriverManager.getConnection(dbURL, username, password);
-            System.out.println("Connected to Oracle database server !");
+            connection = DriverManager.getConnection(dbURL);
+
+            String sql = "INSERT INTO EMPLOYEES (name, age, address, salary) " + "VALUES ('Garnacho', 19, 'Manchester', 2000)";
+            statement = connection.createStatement();
+            int rows = statement.executeUpdate(sql);
+
+            if(rows > 0) {
+                System.out.println("A row has been inserted !");
+            }
+
+            connection.close();
 
         } catch (SQLException e) {
             System.out.println("Ups, something went wrong !");
